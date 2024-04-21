5d384056f9a9406b4174af5cd5aecde93f8b01dd
CavernCap <148848111+CavernCap@users.noreply.github.com>
2024-04-21 20:19:35 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 // Copyright 2024 Kapochkin Mikhail
 
 #include <iostream>
-#include "textgen.h"
 #include <fstream>
+#include "textgen.h"
 const int NPREF = 2; // количество слов в префиксе
 const int MAXGEN = 1000; //объем текста на выходе
 
-int main()
-{
+int main(){
     std::ifstream input_stream("test.txt");
     std::string string, line;
     while (std::getline(input_stream, line)) {
